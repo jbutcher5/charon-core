af75f04173d549c5d0612594feb87367054dab54
James Butcher <jamesbutcher167@gmail.com>
2022-02-13 22:33:55 +0000

    Find bracket positions in w code

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,42 @@ fn has_function(arr: &WCode) -> bool {
     false
 }
 
+fn get_first_bracket_open(arr: &WCode) -> Option<usize> {
+    for (i, token) in arr.iter().enumerate() {
+        match token {
+            Token::Other(value) => {
+                if value == "(" {
+                    return Some(i)
+                } else {
+                    continue
+                }
+            },
+            _ => continue
+        }
+    }
+
+    None
+}
+
+fn get_last_bracket_close(arr: &WCode) -> Option<usize> {
+    let reversed = arr.iter().rev();
+
+    for (i, token) in reversed.enumerate() {
+        match token {
+            Token::Other(value) => {
+                if value == "(" {
+                    return Some(arr.len() - i)
+                } else {
+                    continue
+                }
+            },
+            _ => continue
+        }
+    }
+
+    None
+}
+
 fn sum(data: WCode) -> WCode {
     let nums = as_nums(data);
     vec![Token::Value(nums.iter().sum())]
@@ -47,6 +83,15 @@ fn evaluate(data: WCode) -> WCode {
         },
         None => panic!("No code provided"),
     };
+
+    let first_bracket = get_first_bracket_open(&new_code);
+
+    if first_bracket.is_some() {
+        match get_last_bracket_close(&new_code) {
+            Some(last_index) => unimplemented!(),
+            None => panic!("Unmatched brackets")
+        }
+    }
 
     if has_function(&new_code) {
         return final_function(evaluate(new_code));
@@ -75,5 +120,5 @@ fn lexer(code: &str) -> WCode {
 }
 
 fn main() {
-    println!("{:#?}", lexer("1 2 3 3 ( 4 ) +"));
+    println!("{:#?}", evaluate(lexer("1 2 3 3 ( 4 ) +")));
 }
