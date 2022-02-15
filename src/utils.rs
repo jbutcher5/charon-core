f88e6cc9b27ea2d7e9b6b3c506ca4fad69d80785
James Butcher <jamesbutcher167@gmail.com>
2022-02-15 17:54:45 +0000

    Add bracket pairs function to find pairs of brackets

--- src/utils.rs
+++ src/utils.rs
@@ -15,7 +15,7 @@ pub fn as_wcode(arr: Vec<f64>) -> WCode {
     arr.iter().map(|&value| Token::Value(value)).collect()
 }
 
-pub fn function_pairs(arr: &WCode) -> WFuncPair {
+pub fn outter_function(arr: &WCode) -> WFuncPair {
     let reversed = arr.iter().rev();
 
     let mut results: WFuncPair = (None, None);
@@ -30,11 +30,49 @@ pub fn function_pairs(arr: &WCode) -> WFuncPair {
     for (i, token) in arr.iter().enumerate() {
         match token {
             Token::Function(value) => results.0 = Some((i, *value)),
-            _ => continue
+            _ => continue,
         }
     }
 
     results
+}
+
+pub fn bracket_pairs(arr: &WCode, initial_pos: usize) -> Option<usize> {
+    let mut counter = 0;
+    let mut next_open = 0;
+
+    for (i, token) in arr[initial_pos..].iter().enumerate() {
+        match token {
+            Token::Special(value) => {
+                if value == ")" {
+                    return Some(i);
+                } else if value == "(" {
+                    next_open = initial_pos + i;
+                    counter += 1;
+                }
+            }
+            _ => continue,
+        }
+    }
+
+    for (i, token) in arr[next_open - 1..].iter().enumerate() {
+        match token {
+            Token::Special(value) => {
+                if value == "(" {
+                    counter += 1;
+                } else if value == ")" {
+                    counter -= 1;
+                }
+
+                if counter == 0 {
+                    return Some(i + next_open);
+                }
+            }
+            _ => continue,
+        }
+    }
+
+    None
 }
 
 pub fn get_first_bracket_open(arr: &WCode) -> Option<usize> {
