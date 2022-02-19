8e4b8c09b358a518bc118670741d74ee30c96270
James Butcher <jamesbutcher167@gmail.com>
2022-02-19 21:36:55 +0000

    Add modles for Containers and functions

--- src/modles.rs
+++ src/modles.rs
@@ -3,8 +3,28 @@ pub enum Token {
     Value(f64),
     Function(fn(WCode) -> WCode),
     FunctionLiteral(fn(WCode) -> WCode),
+    Container(String),
+    Parameter(FunctionParameter),
     Atom(String),
     Special(String),
+}
+
+#[derive(Debug, Clone)]
+pub enum FunctionParameter {
+    Exact(usize),
+    Remaining,
+}
+
+#[derive(Debug, Clone)]
+pub struct WSection {
+    pub container: Option<String>,
+    pub code: WCode,
+}
+
+#[derive(Debug, Clone)]
+pub enum WFuncVariant {
+    Container(String),
+    Function(WFunc)
 }
 
 pub type WCode = Vec<Token>;
