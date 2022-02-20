39b90c04c263e76e7bd91d9c0bdaee5124c76bc7
James Butcher <jamesbutcher167@gmail.com>
2022-02-20 13:15:07 +0000

    Move eval functions into evaluator.rs

---
+++ src/evaluator.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+use crate::modles::{WCode, WSection, WFuncVariant};
+use crate::utils::{bracket_pairs, get_first_bracket_open, outter_function, wfunc};
+
+pub fn wsection_eval(data: Vec<WSection>) -> Vec<WCode> {
+    let mut function_map: HashMap<String, WCode> = HashMap::new();
+    let mut result: Vec<WCode> = Vec::new();
+
+    for section in data {
+        match section.container {
+            Some(container) => {
+                function_map.insert(container, section.code);
+            }
+            None => result.push(eval(section.code, &function_map)),
+        }
+    }
+
+    result
+}
+
+pub fn eval(data: WCode, state: &HashMap<String, WCode>) -> WCode {
+    let mut new_code = data.clone();
+
+    let first = get_first_bracket_open(&new_code);
+    let second = match first {
+        Some(x) => bracket_pairs(&new_code, &x),
+        None => None,
+    };
+
+    if first.is_some() && second.is_some() {
+        let (x, y) = (first.unwrap(), second.unwrap());
+        let bracket_code = &data[x + 1..y];
+        new_code.splice(x..y + 1, eval(bracket_code.to_vec(), state));
+
+        if get_first_bracket_open(&new_code).is_some() {
+            new_code = eval(new_code, state)
+        }
+    }
+
+    let funcs = outter_function(&new_code);
+
+    match funcs {
+        (Some((second_func_pos, _)), Some((first_func_pos, func))) => {
+            let code_to_evaluate: WCode = new_code[..first_func_pos].to_vec();
+
+            let result = match func {
+                WFuncVariant::Function(func) => func(code_to_evaluate),
+                WFuncVariant::Container(x) => wfunc(state.get(&x).unwrap(), &code_to_evaluate, state)
+            };
+
+            new_code.splice(..first_func_pos + 1, result);
+
+            if first_func_pos != second_func_pos {
+                new_code = eval(new_code, state);
+            }
+
+            new_code
+        }
+        _ => new_code,
+    }
+}
