894c7d0c8930b49367ada4a342388e9a5102a213
James Butcher <jamesbutcher167@gmail.com>
2022-02-20 12:16:57 +0000

    Refactor previous lexer function to be within section lexer

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,55 @@ use crate::modles::{FunctionParameter, Token, WCode, WFunc, WSection, WFuncVaria
 use crate::stdlib::FUNCTIONS;
 use crate::utils::{as_nums, as_wcode, bracket_pairs, get_first_bracket_open, outter_function, wfunc};
 
-static SPECIALS: phf::Set<&'static str> = phf_set! {
-    ")",
-    "("
-};
-
-fn section_lexer(code: &str) -> Vec<WSection> {
+fn lexer(code: &str) -> Vec<WSection> {
     lazy_static! {
         static ref RE: Regex = Regex::new(" <- ").unwrap();
+        static ref SPECIALS: phf::Set<&'static str> = phf_set! {
+            ")",
+            "("
+        };
+    }
+
+    fn inner(code: &str, containers: &Vec<String>) -> WCode {
+        code.split(' ')
+            .map(|x| match x.parse::<f64>() {
+                Ok(n) => Token::Value(n),
+                Err(_) => {
+                    let cleared = x.chars().filter(|&x| x != '\n').collect::<String>();
+                    let mut chars = cleared.chars();
+
+                    if containers.iter().any(|name| *name == cleared) {
+                        Token::Container(cleared)
+                    } else if cleared.len() > 1 && chars.nth(0).unwrap() == '#' {
+                        if let Ok(index) = cleared[1..].parse::<usize>() {
+                            Token::Parameter(FunctionParameter::Exact(index))
+                        } else if chars.nth(0).unwrap() == 'n' && cleared.len() == 2 {
+                            Token::Parameter(FunctionParameter::Remaining)
+                        } else {
+                            Token::Atom(cleared)
+                        }
+                    } else if cleared.len() > 2
+                        && chars.nth(0).unwrap() == '`'
+                        && chars.last().unwrap() == '`'
+                    {
+                        let function = cleared.substring(1, cleared.len() - 1);
+
+                        Token::FunctionLiteral(
+                            *FUNCTIONS
+                                .get(function)
+                                .unwrap_or_else(|| panic!("Unknown function: {:?}", function)),
+                        )
+                    } else if SPECIALS.contains(&cleared) {
+                        Token::Special(cleared)
+                    } else {
+                        match FUNCTIONS.get(&cleared) {
+                            Some(x) => Token::Function(*x),
+                            None => Token::Atom(x.to_string()),
+                        }
+                    }
+                }
+            })
+            .collect()
     }
 
     let mut containers = vec![];
@@ -35,12 +76,12 @@ fn section_lexer(code: &str) -> Vec<WSection> {
                 containers.push(container.clone());
                 WSection {
                     container: Some(container),
-                    code: lexer(&code, &containers),
+                    code: inner(&code, &containers),
                 }
             }
             None => WSection {
                 container: None,
-                code: lexer(line, &containers),
+                code: inner(line, &containers),
             },
         })
         .collect()
@@ -104,53 +145,13 @@ fn evaluate(data: WCode, state: &HashMap<String, WCode>) -> WCode {
     }
 }
 
-fn lexer(code: &str, containers: &Vec<String>) -> WCode {
-    code.split(' ')
-        .map(|x| match x.parse::<f64>() {
-            Ok(n) => Token::Value(n),
-            Err(_) => {
-                let cleared = x.chars().filter(|&x| x != '\n').collect::<String>();
-                let mut chars = cleared.chars();
 
-                if containers.iter().any(|name| *name == cleared) {
-                    Token::Container(cleared)
-                } else if cleared.len() > 1 && chars.nth(0).unwrap() == '#' {
-                    if let Ok(index) = cleared[1..].parse::<usize>() {
-                        Token::Parameter(FunctionParameter::Exact(index))
-                    } else if chars.nth(0).unwrap() == 'n' && cleared.len() == 2 {
-                        Token::Parameter(FunctionParameter::Remaining)
-                    } else {
-                        Token::Atom(cleared)
-                    }
-                } else if cleared.len() > 2
-                    && chars.nth(0).unwrap() == '`'
-                    && chars.last().unwrap() == '`'
-                {
-                    let function = cleared.substring(1, cleared.len() - 1);
-
-                    Token::FunctionLiteral(
-                        *FUNCTIONS
-                            .get(function)
-                            .unwrap_or_else(|| panic!("Unknown function: {:?}", function)),
-                    )
-                } else if SPECIALS.contains(&cleared) {
-                    Token::Special(cleared)
-                } else {
-                    match FUNCTIONS.get(&cleared) {
-                        Some(x) => Token::Function(*x),
-                        None => Token::Atom(x.to_string()),
-                    }
-                }
-            }
-        })
-        .collect()
-}
 
 fn main() {
-    section_evaluator(section_lexer(
+    section_evaluator(lexer(
         "
 my_sum <- #n sum
-3 8 my_sum OUTPUT
+( 3 8 ) my_sum OUTPUT
 ",
     ));
 }
