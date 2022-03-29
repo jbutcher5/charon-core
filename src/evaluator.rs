8ed93071c39483d69840611758a6ca9803a4d8dd
James Butcher <jamesbutcher167@gmail.com>
2022-03-29 21:51:54 +0100

    Allow infinite recursion with brackets

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -5,6 +5,7 @@ use itertools::Itertools;
 pub trait WEval {
     fn wsection_eval(&mut self, data: Vec<WCode>) -> Vec<WTokens>;
     fn eval(&self, data: WTokens) -> WTokens;
+    fn dissolve(&self, code: &mut WTokens, func: WFuncVariant, first_func_pos: usize, arr: WTokens);
 }
 
 impl WEval for State {
@@ -42,65 +43,76 @@ impl WEval for State {
             };
 
             if let (Some(x), Some(y)) = (first, second) {
-                new_code.splice(x..=y, self.eval(new_code[x+1..y].to_vec()));
+                let mut result = new_code[x + 1..y].to_vec();
+                if let Some((first_func_pos, func)) = last_function(&result) {
+                    let code_to_evaluate = result[..first_func_pos].to_vec();
+                    self.dissolve(&mut result, func, first_func_pos, code_to_evaluate);
+                    new_code.splice(x + 1..y, result);
+                } else {
+                    new_code.splice(x..=y, result);
+                }
             } else {
                 let code_to_evaluate: WTokens = new_code[..first_func_pos].to_vec();
-
-                match func {
-                    WFuncVariant::Function(func) => {
-                        let result = func(code_to_evaluate);
-                        new_code.splice(..first_func_pos + 1, result);
-                    }
-                    WFuncVariant::Container(x) => {
-                        let mut case: WTokens = vec![];
-                        let mut container_acc: WTokens = vec![];
-
-                        for container_case in self.get(&x).unwrap() {
-                            let mut joined = container_case.0.clone();
-                            joined.append(&mut container_case.1.clone());
-                            container_acc.append(&mut joined);
-
-                            let case_prefix =
-                                self.eval(self.apply(&container_case.0, &code_to_evaluate));
-
-                            if case_prefix[0] != Token::Value(0.0) {
-                                case = container_case.1.clone();
-                                break;
-                            }
-                        }
-
-                        let expanded_range = container_acc
-                            .iter()
-                            .filter(|x| matches!(x, Token::Parameter(_)))
-                            .map(|range| match range {
-                                Token::Parameter(Range::Full(full)) => {
-                                    full.clone().collect::<Vec<_>>()
-                                }
-                                Token::Parameter(Range::From(from)) => {
-                                    (0..=from.end).collect::<Vec<_>>()
-                                }
-                                Token::Parameter(Range::To(to)) => {
-                                    (to.start..=code_to_evaluate.len() - 1).collect::<Vec<_>>()
-                                }
-                                _ => panic!(),
-                            })
-                            .flatten()
-                            .unique()
-                            .map(|wlang_index| code_to_evaluate.len() - (wlang_index + 1))
-                            .sorted()
-                            .rev()
-                            .collect::<Vec<usize>>();
-
-                        let result = self.apply(&case, &code_to_evaluate);
-                        new_code.splice(first_func_pos..=first_func_pos, result);
-                        for n in expanded_range {
-                            new_code.remove(n);
-                        }
-                    }
-                }
+                self.dissolve(&mut new_code, func, first_func_pos, code_to_evaluate);
             }
         }
 
         new_code
+    }
+
+    fn dissolve(
+        &self,
+        code: &mut WTokens,
+        func: WFuncVariant,
+        first_func_pos: usize,
+        arr: WTokens,
+    ) {
+        match func {
+            WFuncVariant::Function(func) => {
+                let result = func(arr);
+                code.splice(..first_func_pos + 1, result);
+            }
+            WFuncVariant::Container(x) => {
+                let mut case: WTokens = vec![];
+                let mut container_acc: WTokens = vec![];
+
+                for container_case in self.get(&x).unwrap() {
+                    let mut joined = container_case.0.clone();
+                    joined.append(&mut container_case.1.clone());
+                    container_acc.append(&mut joined);
+
+                    let case_prefix = self.eval(self.apply(&container_case.0, &arr));
+
+                    if case_prefix[0] != Token::Value(0.0) {
+                        case = container_case.1.clone();
+                        break;
+                    }
+                }
+
+                let expanded_range = container_acc
+                    .iter()
+                    .filter(|x| matches!(x, Token::Parameter(_)))
+                    .map(|range| match range {
+                        Token::Parameter(Range::Full(full)) => full.clone().collect::<Vec<_>>(),
+                        Token::Parameter(Range::From(from)) => (0..=from.end).collect::<Vec<_>>(),
+                        Token::Parameter(Range::To(to)) => {
+                            (to.start..=arr.len() - 1).collect::<Vec<_>>()
+                        }
+                        _ => panic!(),
+                    })
+                    .flatten()
+                    .unique()
+                    .map(|wlang_index| arr.len() - (wlang_index + 1))
+                    .sorted()
+                    .rev()
+                    .collect::<Vec<usize>>();
+
+                let result = self.apply(&case, &arr);
+                code.splice(first_func_pos..=first_func_pos, result);
+                for n in expanded_range {
+                    code.remove(n);
+                }
+            }
+        }
     }
 }
