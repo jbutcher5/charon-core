37b247cafedc39ae4670b25a9c2738c2d3764e93
James Butcher <jamesbutcher167@gmail.com>
2022-04-16 14:05:44 +0100

    Add factorial bench mark#

--- benches/core_bench.rs
+++ benches/core_bench.rs
@@ -1,15 +1,23 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use wcore::{evaluator::WEval, models::*};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+fn factorial(n: f64) -> f64 {
+    let code = format!("
+factorial <-|
+  $0 2 < -> 1
+  $0 $0 1 sub factorial mul
+
+{} factorial
+", n);
+    let mut state = State::new();
+    match state.apply(&code)[0][0] {
+        Token::Value(x) => x,
+        _ => panic!("Invalid response!"),
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    c.bench_function("factorial", |b| b.iter(|| factorial(black_box(20.))));
 }
 
 criterion_group!(benches, criterion_benchmark);
