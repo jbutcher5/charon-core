dfa3023e1b6768661a0a8fd59f460b65d87a5973
James Butcher <jamesbutcher167@gmail.com>
2022-05-14 15:32:39 +0100

    Correctly parse lexer tokens

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use crate::lexer::LexerToken as LToken;
-use crate::models::{WCode, State, Token};
+use crate::models::{State, Token, WCode};
 use crate::stdlib::FUNCTIONS;
-use logos::{Span, Logos};
+use logos::{Logos, Span};
 
 pub trait WParser {
     fn parser(&self, code: Vec<(LToken, Span)>, reference: &str) -> Vec<WCode>;
@@ -11,7 +11,11 @@ impl WParser for State {
     fn parser(&self, code: Vec<(LToken, Span)>, reference: &str) -> Vec<WCode> {
         let mut parsed: Vec<WCode> = vec![];
         let mut current_container = WCode::default();
-        let parse = |s: &str| self.parser(LToken::lexer(s).spanned().collect::<Vec<_>>(), s)[0].default_case.clone();
+        let parse = |s: &str| {
+            self.parser(LToken::lexer(s).spanned().collect::<Vec<_>>(), s)[0]
+                .default_case
+                .clone()
+        };
         for (token, span) in code {
             if let LToken::Newline = token {
                 if current_container != WCode::default() {
@@ -32,19 +36,28 @@ impl WParser for State {
             } else if let LToken::Assignment(name) = token {
                 current_container.container = Some(name)
             } else if let LToken::Function(func) = token {
-                if let Some(func_reference) = FUNCTIONS.get(&func)  {
-                    current_container.default_case.push(Token::Function(*func_reference))
+                if let Some(func_reference) = FUNCTIONS.get(&func) {
+                    current_container
+                        .default_case
+                        .push(Token::Function(*func_reference))
                 } else {
                     current_container.default_case.push(Token::Container(func))
                 }
             } else if let LToken::FunctionLiteral(func) = token {
-                if let Some(func_reference) = FUNCTIONS.get(&func)  {
-                    current_container.default_case.push(Token::FunctionLiteral(*func_reference))
+                if let Some(func_reference) = FUNCTIONS.get(&func) {
+                    current_container
+                        .default_case
+                        .push(Token::FunctionLiteral(*func_reference))
                 } else {
-                    current_container.default_case.push(Token::ContainerLiteral(func))
+                    current_container
+                        .default_case
+                        .push(Token::ContainerLiteral(func))
                 }
             }
+        }
 
+        if current_container != WCode::default() {
+            parsed.push(current_container)
         }
 
         parsed
